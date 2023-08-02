f788deed16ff07e58a0a5fc8fda8a5bd9d19fb22
ankitjha <ankitjha20022020@gmail.com>
2023-08-02 22:39:02 +0530

    Minor changes

--- server/server.js
+++ server/server.js
@@ -32,6 +32,6 @@ app.use(cors());
 readdirSync("./routes").map((r) => app.use('/api', require("./routes/" + r)));
 
 // port
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
